6c7f1a0d9ecb45d626cf5e6db4ecdf142c3b1456
Donirvin Dickerson <Dond2018@gmail.com>
2019-08-27 12:13:23 -0700

    fix bugs

--- click-app/src/components/FriendCard/index.js
+++ click-app/src/components/FriendCard/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./style.css";
 
-// This function creates a card whichs hold the props' image and can be click on 
-function FriendCard(props){
-    console.log(props)
+function FriendCard(props) {
+  console.log(props)
+  return (
+    <div className="card">
+      <div className="img-container">
+        <img alt={props.name} src={props.image} onClick={() => props.onclickcard(props.id)} />
+      </div>
     
-    return(
-        <div className="card">
-            <div className="img-container">
-                <img alt={props.name} src={props.image} onClick={() => props.onclickcard(props.id)}/>
-            </div>
-        </div>
-    );
+    </div>
+  );
 }
 
 export default FriendCard;
